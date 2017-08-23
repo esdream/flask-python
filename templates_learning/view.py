ec9093c552e8ba70fceddd4a05d241fe817a0916
Faizal <conquerfu@gmail.com>
2017-08-23 10:46:38 +0800

    'inheritation_finished'

--- templates_learning/view.py
+++ templates_learning/view.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -8,9 +10,9 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    mydict = { 'key': 'Lucio' }
-    mylist = [324, 'sdf', True, (2, 4)]
-    return render_template('user.html', name=name, mylist=mylist, mydict=mydict)
+    # mydict = { 'key': 'Lucio' }
+    # mylist = [324, 'sdf', True, (2, 4)]
+    return render_template('user_bootstrap.html', name=name)
 
 if(__name__ == '__main__'):
     app.run(debug=True)
