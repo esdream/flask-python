1fea1e9fab1a460b53f3d7249a499e6822cffce6
Faizal <conquerfu@gmail.com>
2017-08-29 23:34:34 +0800

    'form_class_WIP2'

--- web_form/view.py
+++ web_form/view.py
@@ -6,6 +6,10 @@ from wtforms.validators import Required
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 @app.route('/')
 def index():
     print(app.config)
