f30bd698643b2858946654b33fb11ebbb2f4a844
Faizal <conquerfu@gmail.com>
2017-08-31 23:39:12 +0800

    'form_finished;

--- web_form/view.py
+++ web_form/view.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_wtf import Form
+from flask_bootstrap import Bootstrap
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    print(app.config)
-    return ''
+    name = None
+    form = NameForm()
+    if(form.validate_on_submit):
+        name = form.name.data
+        form.name.data = ''
+        print(name)
+    return render_template('index.html', form=form, name=name)
 
 if(__name__ == '__main__'):
     app.run(debug=True)
